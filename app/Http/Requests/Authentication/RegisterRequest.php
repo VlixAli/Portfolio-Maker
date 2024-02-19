fea469a4cdcc41addf0fe2daa4ce53b93421fe62
Ali <qali372@gmail.com>
2024-02-19 21:17:08 +0200

    build : RegisterRequest.php with validation rules

---
+++ app/Http/Requests/Authentication/RegisterRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests\Authentication;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'first_name' => 'required|min:3|max:255',
+            'last_name' => 'required|min:3|max:255',
+            'username' => 'required|min:3|max:255|unique:users,username',
+            'email' => 'required|email|max:255|unique:users,email',
+            'password' => 'required|confirmed|min:7|max:255',
+            'image' => 'image|max:1048576|dimensions:min_width=100,min_height=100'
+        ];
+    }
+}
