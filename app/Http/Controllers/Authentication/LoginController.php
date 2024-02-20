88d490b462a84beded33ee3da41e91ceefe1e7bd
Ali <qali372@gmail.com>
2024-02-20 07:50:44 +0200

    feat : implement login in LoginController.php

--- app/Http/Controllers/Authentication/LoginController.php
+++ app/Http/Controllers/Authentication/LoginController.php
@@ -3,12 +3,32 @@
 namespace App\Http\Controllers\Authentication;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Authentication\LoginRequest;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class LoginController extends Controller
 {
     public function create()
     {
         return view('login.create');
+    }
+
+    public function store(LoginRequest $request)
+    {
+        $credentials = [
+            'email' => $request->validated('email'),
+            'password' => $request->validated('password')
+        ];
+
+        if(!auth()->attempt($credentials)){
+            throw ValidationException::withMessages([
+                'email' => 'your provided credentials aren not correct'
+            ]);
+        }
+
+        session()->regenerate();
+
+        return redirect('/')->with('success', 'Welcome Back!');
     }
 }
