d88aa6d1534b28534a7093b00932a7176f9c7e53
Ali <qali372@gmail.com>
2024-02-20 09:28:59 +0200

    refactor : add names to register endpoints

--- routes/register.php
+++ routes/register.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\Authentication\RegisterController;
 use Illuminate\Support\Facades\Route;
 
-Route::controller( RegisterController::class)->group(function (){
-    Route::get('register', 'create');
-    Route::post('register' , 'store');
+Route::controller( RegisterController::class)->name('register.')->group(function (){
+    Route::get('register', 'create')->name('create');
+    Route::post('register' , 'store')->name('store');
 });
