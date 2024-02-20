ba6a835a4cd170907c54e887b276459718b61db4
Ali <qali372@gmail.com>
2024-02-20 15:02:54 +0200

    feat : adding name to portfolio index endpoint

--- routes/portfolio.php
+++ routes/portfolio.php
@@ -4,6 +4,6 @@ use App\Http\Controllers\Portfolio\PortfolioController;
 use Illuminate\Support\Facades\Route;
 
 Route::controller(PortfolioController::class)
-    ->middleware('auth')->group(function () {
-        Route::get('/home', 'index');
+    ->middleware('auth')->name('portfolio.')->group(function () {
+        Route::get('/home', 'index')->name('index');
     });
