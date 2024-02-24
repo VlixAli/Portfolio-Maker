8ebe7a07fc371423080b2195c3e303b579eb1f62
Ali <qali372@gmail.com>
2024-02-24 20:23:51 +0200

    feat : return titles back to the index view in PortfolioController.php

--- app/Http/Controllers/Portfolio/PortfolioController.php
+++ app/Http/Controllers/Portfolio/PortfolioController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers\Portfolio;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class PortfolioController extends Controller
 {
     public function index()
     {
-        return view('portfolio.index');
+        return view('portfolio.index',[
+            'titles' => auth()->user()->professionsString
+        ]);
     }
 }
