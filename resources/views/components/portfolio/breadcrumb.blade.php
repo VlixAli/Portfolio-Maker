62f7ad9b1205bccb08dedfe1ea36abe8d4e41845
Ali <qali372@gmail.com>
2024-02-28 16:58:41 +0200

    refactor : extract breadcrumb.blade.php component

---
+++ resources/views/components/portfolio/breadcrumb.blade.php
@@ -0,0 +1,17 @@
+@props([
+    'title' => 'Inner Page'
+])
+
+<!-- ======= Breadcrumbs ======= -->
+<section class="breadcrumbs">
+    <div class="container">
+
+        <div class="d-flex justify-content-between align-items-center">
+            <h2>{{ $title }}</h2>
+            <ol>
+                <li><a href="../../portfolio/index.blade.php">Home</a></li>
+                <li>{{ $title }}</li>
+            </ol>
+        </div>
+    </div>
+</section><!-- End Breadcrumbs -->
