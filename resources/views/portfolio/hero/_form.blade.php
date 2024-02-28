ea26acd4e72c509dace139eafb58142e2d6c74a6
Ali <qali372@gmail.com>
2024-02-28 17:45:15 +0200

    build : portfolio home _form.blade.php

---
+++ resources/views/portfolio/hero/_form.blade.php
@@ -0,0 +1,16 @@
+@if($errors->any())
+    <div class="alert alert-danger">
+        <h3>Error Occurred!</h3>
+        <ul>
+            @foreach($errors->all() as $error)
+                <li>{{$error}}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+<div class="form-group mb-3">
+    <x-form2.input label="New Title" name="title" placeholder="Add your new Title!"/>
+</div>
+<div class="form-group mb-3">
+    <button type="submit" class="btn btn-primary">{{ $button_label ?? 'Save' }}</button>
+</div>
