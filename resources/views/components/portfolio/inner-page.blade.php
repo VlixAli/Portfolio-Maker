497d18a7d11aec9b3c743b9ab750f54e89821490
Ali <qali372@gmail.com>
2024-02-28 17:46:39 +0200

    feat : add user and image variables to inner-page.blade.php component

--- resources/views/components/portfolio/inner-page.blade.php
+++ resources/views/components/portfolio/inner-page.blade.php
@@ -1,11 +1,15 @@
 @props([
-    'title' => 'Inner Page2'
+    'title' => 'Inner Page2',
+    'user' => 'anonymous' ,
+    'image' => 'assets/img/profile-img.jpg'
 ])
 
 <x-portfolio.layout>
-    <x-portfolio.navbar />
+    <x-portfolio.navbar :user="$user" :image="$image" />
     <main id="main">
         <x-portfolio.breadcrumb :title="$title" />
+        <div class="mt-3 mx-3">
         {{ $slot }}
+        </div>
     </main><!-- End #main -->
 </x-portfolio.layout>
