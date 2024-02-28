e39929b2ab05f4b563eb2b88821ce3d8e894fb6f
Ali <qali372@gmail.com>
2024-02-28 20:51:25 +0200

    feat : implement store() in HeroController.php

--- app/Http/Controllers/Portfolio/HeroController.php
+++ app/Http/Controllers/Portfolio/HeroController.php
@@ -3,12 +3,29 @@
 namespace App\Http\Controllers\Portfolio;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Hero\StoreRequest;
+use App\Models\Profession;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HeroController extends Controller
 {
+//    public function index()
+//    {
+//        return view('portfolio.hero.create');
+//    }
     public function create()
     {
         return view('portfolio.hero.create');
+    }
+
+    public function store(StoreRequest $request)
+    {
+        Profession::create([
+            'name' => $request->validated('name'),
+            'user_id' => $request->user()->id,
+        ]);
+
+        return redirect()->route('portfolio.index')->with('success', 'Title Added!');
     }
 }
