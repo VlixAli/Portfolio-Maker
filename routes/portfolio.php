2f49d934006d72c715b1e120708e15848264f543
Ali <qali372@gmail.com>
2024-02-28 17:56:59 +0200

    feat : include hero.php routes file in portfolio.php

--- routes/portfolio.php
+++ routes/portfolio.php
@@ -6,4 +6,6 @@ use Illuminate\Support\Facades\Route;
 Route::controller(PortfolioController::class)
     ->middleware('auth')->name('portfolio.')->group(function () {
         Route::get('/home', 'index')->name('index');
+
+        include 'hero.php';
     });
