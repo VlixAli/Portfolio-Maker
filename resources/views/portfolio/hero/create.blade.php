fd4a6a707eb569f506f7953ad1514d8004cf1c60
Ali <qali372@gmail.com>
2024-02-28 20:54:10 +0200

    feat : make the form action on the same page on hero create.blade.php

--- resources/views/portfolio/hero/create.blade.php
+++ resources/views/portfolio/hero/create.blade.php
@@ -1,5 +1,6 @@
+@php use Illuminate\Support\Facades\URL; @endphp
 <x-portfolio.inner-page title="Add Title" :user="auth()->user()->fullName">
-    <form action="" method="post">
+    <form action="{{ URL::current() }}" method="post">
         @csrf
 
         @include('portfolio.hero._form')
