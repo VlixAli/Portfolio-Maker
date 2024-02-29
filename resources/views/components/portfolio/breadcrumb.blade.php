712d82d905567cfc30d296da0926603410909385
Ali <qali372@gmail.com>
2024-02-29 09:02:07 +0200

    fix : home link in breadcrumb.blade.php component

--- resources/views/components/portfolio/breadcrumb.blade.php
+++ resources/views/components/portfolio/breadcrumb.blade.php
@@ -9,7 +9,7 @@
         <div class="d-flex justify-content-between align-items-center">
             <h2>{{ $title }}</h2>
             <ol>
-                <li><a href="../../portfolio/index.blade.php">Home</a></li>
+                <li><a href="{{route('portfolio.index')}}">Home</a></li>
                 <li>{{ $title }}</li>
             </ol>
         </div>
