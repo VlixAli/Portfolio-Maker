59ced904d942997dabaa98367df2cf015ba3951a
Ali <qali372@gmail.com>
2024-02-29 09:43:27 +0200

    feat : add endpoint to edit title in hero.php

--- routes/hero.php
+++ routes/hero.php
@@ -8,4 +8,5 @@ Route::controller(HeroController::class)
         Route::get('/index', 'index')->name('index');
         Route::get('/hero', 'create')->name('create');
         Route::post('/hero', 'store')->name('store');
+        Route::get('/hero/{title:slug}', 'edit')->name('edit');
     });
