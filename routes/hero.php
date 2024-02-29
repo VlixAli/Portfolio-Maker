65801002e57632e9abf4a526ef6896a608512aae
Ali <qali372@gmail.com>
2024-02-29 09:02:42 +0200

    feat : add index endpoint to hero.php

--- routes/hero.php
+++ routes/hero.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::controller(HeroController::class)
     ->name('hero.')->group(function () {
+        Route::get('/index', 'index')->name('index');
         Route::get('/hero', 'create')->name('create');
         Route::post('/hero', 'store')->name('store');
     });
