b9c2ed5740fb2110e5c6091c1d185b12251b154f
Ali <qali372@gmail.com>
2024-06-02 12:53:54 +0300

    feat : add return type BelongsToMany to user relationship in Title.php model

--- app/Models/Title.php
+++ app/Models/Title.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Title extends Model
 {
@@ -14,7 +15,7 @@ class Title extends Model
         'slug'
     ];
 
-    public function users()
+    public function users() : BelongsToMany
     {
         return $this->belongsToMany(User::class);
     }
