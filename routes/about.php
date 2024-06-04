34a47aa2838f5f5d2e6301e578eb6f1559ef1e6e
Ali <qali372@gmail.com>
2024-06-04 15:03:58 +0300

    refactor : about.php and hero.php routes files to be more clean

--- routes/about.php
+++ routes/about.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\Portfolio\AboutController;
 use Illuminate\Support\Facades\Route;
 
 Route::controller(AboutController::class)
-    ->name('about.')->group(function () {
-        Route::get('/about/{about}', 'edit')->name('edit');
-        Route::put('/about/{user:id}', 'update')->name('update');
+    ->name('about.')->prefix('/about')->group(function () {
+        Route::get('/{about}', 'edit')->name('edit');
+        Route::put('/{user:id}', 'update')->name('update');
     });
--- routes/hero.php
+++ routes/hero.php
@@ -4,12 +4,12 @@ use App\Http\Controllers\Portfolio\HeroController;
 use Illuminate\Support\Facades\Route;
 
 Route::controller(HeroController::class)
-    ->name('hero.')->group(function () {
+    ->name('hero.')->prefix('/hero')->group(function () {
         Route::get('/index', 'index')->name('index');
-        Route::get('/hero', 'create')->name('create');
-        Route::post('/hero', 'store')->name('store');
-        Route::get('/hero/{title:slug}', 'edit')->name('edit');
-        Route::post('/hero/{title:slug}', 'update')->name('update');
-        Route::delete('/hero/{title:slug}', 'destroy')->name('delete');
+        Route::get('/', 'create')->name('create');
+        Route::post('/', 'store')->name('store');
+        Route::get('/{title:slug}', 'edit')->name('edit');
+        Route::post('/{title:slug}', 'update')->name('update');
         Route::delete('/index', 'empty')->name('empty');
+        Route::delete('/{title:slug}', 'destroy')->name('delete');
     });
