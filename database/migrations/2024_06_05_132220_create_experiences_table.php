6ada9baa880860a7dec5bf4138e59d70c1f2b4bf
Ali <qali372@gmail.com>
2024-06-06 18:40:25 +0300

    build : experience migration class

---
+++ database/migrations/2024_06_05_132220_create_experiences_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('experiences', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->string('position');
+            $table->integer('starting_year');
+            $table->integer('ending_year');
+            $table->string('company');
+            $table->longText('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('experiences');
+    }
+};
